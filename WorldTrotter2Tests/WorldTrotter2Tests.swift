79cc7c557b70fe41175c119c9e45095b631f576f
TheMAZ2-png <csm012004@outlook.com>
2025-09-30 16:09:22 -0400

    Initial Commit

---
+++ WorldTrotter2Tests/WorldTrotter2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTrotter2Tests.swift
+//  WorldTrotter2Tests
+//
+//  Created by Chris Mazurek on 9/30/25.
+//
+
+import Testing
+@testable import WorldTrotter2
+
+struct WorldTrotter2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ WorldTrotter2UITests/WorldTrotter2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  WorldTrotter2UITestsLaunchTests.swift
+//  WorldTrotter2UITests
+//
+//  Created by Chris Mazurek on 9/30/25.
+//
+
+import XCTest
+
+final class WorldTrotter2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
