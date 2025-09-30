fb1ce31784846bbfc18f2c933c01c008d0069590
TheMAZ2-png <csm012004@outlook.com>
2025-09-30 18:55:41 -0400

    jlklkjk

--- WorldTrotter2/ConversionViewController.swift
+++ WorldTrotter2/ConversionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
@@ -52,6 +52,23 @@ class ConversionViewController: UIViewController {
             celsiusLabel.text = "???"
         }
     }
+    
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+
+        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
+            let replacementTextHasDecimalSeparator = string.range(of: ".")
+
+            if existingTextHasDecimalSeparator != nil,
+                replacementTextHasDecimalSeparator != nil {
+                return false
+            } else {
+                return true
+            }
+    }
+
     
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         if let text = textField.text, let value = Double(text) {
